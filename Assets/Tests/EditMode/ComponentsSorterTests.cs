2a14737e91e98d450707410a8966df44fc09030d
danineuss <daniel.neuss@hey.com>
2022-04-07 17:30:20 +0200

    ComponentsSorterTests: clean dummy code.

--- Assets/Tests/EditMode/ComponentsSorterTests.cs
+++ Assets/Tests/EditMode/ComponentsSorterTests.cs
@@ -4,62 +4,8 @@ using UnityEngine;
 
 namespace Tests.EditMode
 {
-    internal class Foo
-    {
-        public int Count => _gameObjects.Count;
-
-        private readonly List<GameObject> _gameObjects;
-
-        public Foo(List<GameObject> gameObjects)
-        {
-            _gameObjects = gameObjects;
-        }
-    }
-
     public class ComponentsSorterTests
     {
-        [Test]
-        public void Init_ShouldDisplayCorrectTestCount()
-        {
-            // Arrange
-            var gameObjectList = new List<GameObject>
-            {
-                new GameObject("a"),
-                new GameObject("b"),
-                new GameObject("c"),
-                new GameObject("d"),
-                new GameObject("e")
-            };
-
-            // Act
-            var foo = new Foo(gameObjectList);
-
-            // Assert
-            Assert.AreEqual(foo.Count, 5);
-        }
-
-        //[Test]
-        //public void Init_ShouldDisplayCorrectTestName()
-        //{
-
-        //    // Arrange
-        //    var gameObjectList = new List<GameObject>
-        //    {
-        //        new GameObject("a"),
-        //        new GameObject("b"),
-        //        new GameObject("c"),
-        //        new GameObject("d"),
-        //        new GameObject("e")
-        //    };
-        //    IBarable barable = Substitute.For<IBarable>();
-        //    barable.Name.Returns("Bar");
-
-        //    // Act
-        //    var foo = new Foo(gameObjectList, barable);
-
-        //    // Assert
-        //    Assert.AreEqual(foo.Count, 5);
-        //    Assert.AreEqual(foo.Name, "Bar");
-        //}
+        
     }
 }
